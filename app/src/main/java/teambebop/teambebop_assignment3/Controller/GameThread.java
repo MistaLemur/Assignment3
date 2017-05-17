d00dc700ec9d9e2d4d91ab6dfdb653a0741a2e8d
Byron <buaguila@uci.edu>
2017-05-17 16:47:41 -0700

    Making files

---
+++ app/src/main/java/teambebop/teambebop_assignment3/Controller/GameThread.java
@@ -0,0 +1,8 @@
+package teambebop.teambebop_assignment3.Controller;
+
+/**
+ * Created by Byron on 5/17/2017.
+ */
+
+public class GameThread {
+}
---
+++ app/src/main/java/teambebop/teambebop_assignment3/Model/DigDug.java
@@ -0,0 +1,8 @@
+package teambebop.teambebop_assignment3.Model;
+
+/**
+ * Created by Byron on 5/17/2017.
+ */
+
+public class DigDug {
+}
