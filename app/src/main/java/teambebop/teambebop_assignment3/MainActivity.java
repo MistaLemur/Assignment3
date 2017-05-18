fbb60de6d834dd6eb7e2fecbf18d8ca172bf2134
D4RK354B3R <d4rk3.54b3r@gmail.com>
2017-05-17 17:24:35 -0700

    added program entry, MainActivity

---
+++ app/src/main/java/teambebop/teambebop_assignment3/MainActivity.java
@@ -0,0 +1,17 @@
+package teambebop.teambebop_assignment3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import teambebop.teambebop_assignment3.View.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GameView gameView = new GameView(this);
+        setContentView(gameView);
+        setContentView(R.layout.activity_main);
+    }
+}
