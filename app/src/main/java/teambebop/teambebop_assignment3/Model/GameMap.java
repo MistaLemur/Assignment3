c9e8213637f47f82951d47e708f4294e6dc65e55
D4RK354B3R <d4rk3.54b3r@gmail.com>
2017-05-18 21:17:58 -0700

    Working on quadtree

--- app/src/main/java/teambebop/teambebop_assignment3/Model/GameMap.java
+++ app/src/main/java/teambebop/teambebop_assignment3/Model/GameMap.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
  */
 
 /*
-The GameMap class, in order to achieve a high granularity when digging tunnels, implements a quadtree
-to keep track of the dirt.
+The GameMap class, in order to achieve a high granularity when digging tunnels,
+implements a quadtree to keep track of the dirt and the tunnels.
 The GameMap only keeps track of dirt, and has nothing to do with the characters that might inhabit the space.
+The GameMap class also contains functions for collision checking shapes with solid dirt.
+    This is needed for obstruction of monster movement, and digging.
 */
 
 
@@ -17,6 +19,16 @@ public class GameMap {
 
     QuadTreeNode quadTreeRoot;
 
+
+    public boolean collideDirtRect(int ax1, int ay1, int ax2, int ay2){
+        //This function will return true if the given rectangle collides with ANY dirt.
+
+    }
+
+    public boolean collideDirtCircle(int ax1, int ay1, int r){
+        //This function will return true if the given circle collides with ANY dirt.
+
+    }
 }
 
 
@@ -35,13 +47,76 @@ class QuadTreeNode{
      1  indicates filled; this whole node is made up of solid dirt
      */
 
+    public QuadTreeNode(int nx1, int nx2, int ny1, int ny2, QuadTreeNode nParent){
+        x1 = nx1;
+        y1 = ny1;
+        x2 = nx2;
+        y2 = ny2;
+
+        if(nParent != null){
+            parent = nParent;
+            parent.children.add(this);
+
+            isFilledIn = parent.isFilledIn;
+        }
+    }
+
     public void subdivide(){
         //this function subdivides this node into four children
+        if(!canSubdivide()) return;
+
+        int w = x2-x1;
+        int h = y2-y1;
+
+        QuadTreeNode child;
+        //top left
+        new QuadTreeNode(x1, y1, x1+w/2, y1+h/2, this);
+        //top right
+        new QuadTreeNode(x1 + w/2 + 1, y1, x1+w, y1+h/2, this);
+        //bottom left
+        new QuadTreeNode(x1, y1+h/2 + 1, x1+w/2, y1+h, this);
+        //bottom right
+        new QuadTreeNode(x1 + w/2 + 1, y1 + h/2 + 1, x1+w, y1+h, this);
+    }
+
+    public boolean canSubdivide(){
+        if(Math.abs(x2-x1) <= 1 || Math.abs(y2-y1) <= 1) return false;
+        return true;
     }
 
     public void mergeChildren(){
         //this function checks to see if all children are uniform;
         //if they are, the children are removed and "merged" into this object
+
+        if(children.size() == 0) return; //there are no children to merge...
+
+        //first check if the children have children. If so, then pass the function call to the children
+        //BEFORE merging this.
+        for(QuadTreeNode child: children){
+            //if the child also has children, pass the merge call to them
+            if(child.children.size() > 0){
+                child.mergeChildren();
+            }
+        }
+
+        int filledValue = -2;
+        boolean isUniform = true;
+        for(QuadTreeNode child: children){
+
+            if(filledValue == -2){
+                filledValue = child.isFilledIn;
+            }else if(child.isFilledIn != filledValue){
+                isUniform = false;
+                break;
+            }
+        }
+
+        if(isUniform && filledValue >= 0){
+            //oh shit merge that fucking shit.
+            //remove the children.
+            children.clear();
+            isFilledIn = filledValue;
+        }
     }
 
     public int collidesRect(int ax1, int ay1, int ax2, int ay2){
@@ -66,22 +141,63 @@ class QuadTreeNode{
     public void digTunnelRect(int ax1, int ay1, int ax2, int ay2){
         //This function will dig a tunnel in the shape of the given rectangle
 
-        //if I have children, just recursively function call on the children
+        //if I have children, just recursively function call on the children and then return
+        if(children.size() > 0){
+            for(QuadTreeNode child:children){
+                digTunnelRect(ax1, ay1, ax2, ay2);
+            }
+
+            return;
+        }
 
         //if I don't have children, run the collision check
+        int collision = collidesRect(ax1, ay1, ax2, ay2);
+        if(collision == 0) return; //the shape does not have anything to do with this node.
+
         //if the shape contains all of me or I cannot subdivide, then set my value to "empty"
-        //if intersects OR I contain the entirety of the given shape, then subdivide and recurse.
+        if(collision == 3 || !canSubdivide()){
+            isFilledIn = 0;
+        }else{
+            //if intersects OR I contain the entirety of the given shape, then subdivide and recurse.
+            subdivide();
+            isFilledIn = -1;
+
+            for(QuadTreeNode child:children){
+                digTunnelRect(ax1, ay1, ax2, ay2);
+            }
+        }
 
     }
 
     public void digTunnelCircle(int ax1, int ay1, int r){
-        //This function will dig a tunnel in the shape of the given circle
+        //This function will dig a tunnel in the shape of the given rectangle
 
-        //if I have children, just recursively function call on the children
+        //if I have children, just recursively function call on the children and then return
+        if(children.size() > 0){
+            for(QuadTreeNode child:children){
+                digTunnelCircle(ax1, ay1, r);
+            }
+
+            return;
+        }
 
         //if I don't have children, run the collision check
+        int collision = collidesCircle(ax1, ay1, r);
+        if(collision == 0) return; //the shape does not have anything to do with this node.
+
         //if the shape contains all of me or I cannot subdivide, then set my value to "empty"
-        //if intersects OR I contain the entirety of the given shape, then subdivide and recurse.
+        if(collision == 3 || !canSubdivide()){
+            isFilledIn = 0;
+        }else{
+            //if intersects OR I contain the entirety of the given shape, then subdivide and recurse.
+            isFilledIn = -1;
+            subdivide();
+
+            for(QuadTreeNode child:children){
+                digTunnelCircle(ax1, ay1, r);
+            }
+        }
+
     }
 }
 
@@ -91,6 +207,7 @@ class collisions{
      All rectangles here are AABBs (axis-aligned bounding boxes)
      It will have:
      - Point in Rect
+     - Line intersects Line
      - Rect in Rect
      - Rect intersects Rect
      - Line intersects Circle
